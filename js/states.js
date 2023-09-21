7c5be3dcda8e43a9fea78dc85095d49fafee3bf2
BasD <basdoppen@gmail.com>
2023-09-21 09:12:59 +0200

    facets + object narrative

--- js/states.js
+++ js/states.js
@@ -1,3 +1,5 @@
+//const { log } = require("handlebars")
+
 function setState(id, state) {
     let toSetStates = []
     for (const [key, value] of Object.entries(states)) {
@@ -8,14 +10,14 @@ function setState(id, state) {
         }
         // remove states
         value.forEach((item) => {
-           handleClass(id, 'remove', item)
+            handleClass(id, 'remove', item)
         })
-      }
+    }
 
-        // add styles
-        toSetStates.forEach((item) => {
-            handleClass(id, 'add', item)
-        })
+    // add styles
+    toSetStates.forEach((item) => {
+        handleClass(id, 'add', item)
+    })
 
 }
 
@@ -56,7 +58,7 @@ function openInfoMeta() {
     handleClass('info1', 'add', 'flex');
     handleClass('info1', 'remove', 'hidden')
 
-    
+
 }
 
 function openEdit() {
@@ -66,13 +68,13 @@ function openEdit() {
 
     handleClass('form1', 'add', 'flex');
     handleClass('form1', 'remove', 'hidden')
-    
+
 }
 
 function openCommunityFilters() {
     handleClass('communityFilters', 'add', 'flex');
     handleClass('communityFilters', 'remove', 'hidden')
-    
+
 }
 
 function openInfoThesaurus() {
@@ -82,7 +84,7 @@ function openInfoThesaurus() {
 
     handleClass('info2', 'add', 'flex');
     handleClass('info2', 'remove', 'hidden')
-    
+
 }
 
 function closeAside() {
@@ -98,4 +100,21 @@ function openZoom() {
 function closeZoom() {
     handleClass('imageZoom', 'add', 'hidden');
     handleClass('imageZoom', 'remove', 'flex')
+}
+
+function showResource() {
+    handleClass('showResource', 'add', 'flex');
+    handleClass('showResource', 'remove', 'hidden')
+}
+
+function openAddNarrForm(elemId) {
+    console.log(elemId);
+    handleClass(elemId, 'add', 'flex');
+    handleClass(elemId, 'remove', 'hidden')
+}
+
+function closeAddNarrForm(elemId) {
+    console.log(elemId);
+    handleClass(elemId, 'add', 'hidden');
+    handleClass(elemId, 'remove', 'flex')
 }
